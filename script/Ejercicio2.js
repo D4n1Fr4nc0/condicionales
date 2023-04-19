c2b352fe1e91279a02404e2a241ae4620feb2332
D4n1Franc0 <danielarodriguezfranco@iesantaelena.edu.co>
2023-04-19 16:21:04 -0500

    cambio

--- script/Ejercicio2.js
+++ script/Ejercicio2.js
@@ -1,7 +1,15 @@
-let edad, niño, preadolecente, adolecente, adulto, adultoM;
+let edad;
 
 edad = Number(prompt('ingrese la edad '))
 
 if ( edad < 10){
     alert('es un niño')
-} else if ( edad >=10 && )
+} else if ( edad >=10 && edad <15 ){
+    alert('es preadolecente')
+} else if ( edad >=15 && edad <18){
+    alert('es un adolecente')
+} else if (edad >=18 && edad <50){
+    alert('es un adulto')
+} else if (edad >= 50){
+    alert('es adulto mayor')
+}
