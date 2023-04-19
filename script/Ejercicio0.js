f22df723daccd47cc3fad616a1b53b9f3fd064d7
D4n1Franc0 <danielarodriguezfranco@iesantaelena.edu.co>
2023-04-19 16:02:18 -0500

    ejercicios

---
+++ script/Ejercicio0.js
@@ -0,0 +1,20 @@
+let cantidad, descuento=0, monto, totalPagar 
+
+//leer o asignar informacion
+cantidad = Number(prompt('ingrese cantidad'))
+
+//proceso
+monto = cantidad * 2000000;
+
+if (cantidad<10) {
+    alert('no tiene descuento')
+} else if(cantidad > 10 && cantidad < 20) {
+    descuento=monto * 0.1
+} else if(cantidad >= 20 && cantidad < 30){
+    descuento=monto * 0.2
+} else if(cantidad >= 30){
+    descuento=monto * 0.4
+}
+
+totalPagar= monto - descuento;
+alert('el total a pagar es ' + totalPagar)
---
+++ script/Ejercicio2.js
@@ -0,0 +1,7 @@
+let edad, niño, preadolecente, adolecente, adulto, adultoM;
+
+edad = Number(prompt('ingrese la edad '))
+
+if ( edad < 10){
+    alert('es un niño')
+} else if ( edad >=10 && )
